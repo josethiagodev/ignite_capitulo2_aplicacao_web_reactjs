11def3747ba809c4693e4fc1ed31ef2a7dabb357
Jose Thiago <josethiagowd@gmail.com>
2021-03-19 01:27:12 -0300

    Consumindo API (MirageJS)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from "miragejs";
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    // GET = Rota de buscar/listar itens
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: '',
+          amount: 400,
+          type: 'deposito',
+          category: 'Entrega',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
